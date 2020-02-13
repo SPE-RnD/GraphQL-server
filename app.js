f524499d071ed799437df6c8d0c83254542bde06
Calvin <calvin.johannes@spesolution.com>
2020-02-13 13:53:33 +0700

    update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express'); //Include Express
 const app = express(); //Mengaktifkan Express
 const graphqlHTTP = require('express-graphql'); //Include Express GraphQL
-// const schemafilemysql = require('./schema/schemamysql'); //File Schema Dengan Data Dari Database MySQL
-// const schemafilepostgre = require('./schema/schemapostgre'); //File Schema Dengan Data Dari Database PostgreSQL
-// const schemafilemongo = require('./schema/schemamongo'); //File Schema Dengan Data Dari Database PostgreSQL
+const schemafilemysql = require('./schema/schemamysql'); //File Schema Dengan Data Dari Database MySQL
+const schemafilepostgre = require('./schema/schemapostgre'); //File Schema Dengan Data Dari Database PostgreSQL
+const schemafilemongo = require('./schema/schemamongo'); //File Schema Dengan Data Dari Database PostgreSQL
 const cors = require('cors'); //Include Cors
 const schemafile = require('./schema/schema'); //File Schema Dengan Data Dummy
 const schemafilemysqlpool = require('./schema/schemamysqlpool'); //File Schema Dengan Data Dari Database MySQL
@@ -14,10 +14,10 @@ const {graphql} = require('graphql'); //include GraphQL
 app.use(cors()); //Jalanin Cors
 
 //Link GraphQL Dengan Data Dari DB Mysql 
-// app.use('/graphmysql', graphqlHTTP({
-//     schema:schemafilemysql, //File Schema Yang Dituju
-//     graphiql:true, //Untuk mengaktifkan GUI GraphQL pada WEB 
-// }));
+app.use('/graphmysql', graphqlHTTP({
+    schema:schemafilemysql, //File Schema Yang Dituju
+    graphiql:true, //Untuk mengaktifkan GUI GraphQL pada WEB 
+}));
 //------------------------------
 
 //Link GraphQL Dengan Data Dari DB Mysql 
@@ -27,41 +27,41 @@ app.use('/graphmysqlpool', graphqlHTTP({
 }));
 //------------------------------
 
-//Untuk Encrypt
-// function run_query(str){
-//     return graphql(schemafilemysql,str); //menjalankan graphql
-// }
+// Untuk Encrypt
+function run_query(str){
+    return graphql(schemafilemysql,str); //menjalankan graphql
+}
 
-// app.use('/graphmysqlcrypt',(req,res)=>{
-//         var str_query = Buffer.from(req.query.query, 'base64').toString('ascii'); //decrypt query
-//         run_query(str_query).then(data => { //Menjalankan graphQL
-//             var hasil = Buffer.from(JSON.stringify(data)).toString('base64'); //encrypt response dari graphQL
-//             res.send(hasil); //mengembalikan hasil
-//         });
-// });
-// //--------------------------------------------------------------------------------
+app.use('/graphmysqlcrypt',(req,res)=>{
+        var str_query = Buffer.from(req.query.query, 'base64').toString('ascii'); //decrypt query
+        run_query(str_query).then(data => { //Menjalankan graphQL
+            var hasil = Buffer.from(JSON.stringify(data)).toString('base64'); //encrypt response dari graphQL
+            res.send(hasil); //mengembalikan hasil
+        });
+});
+//--------------------------------------------------------------------------------
 
-// //Link GraphQL Dengan Data Dari DB Postgre 
-// app.use('/graphpostgre', graphqlHTTP({
-//     schema:schemafilepostgre, //File Schema Yang Dituju
-//     graphiql:true, //Untuk mengaktifkan GUI GraphQL pada WEB 
-// }));
-// //------------------------------
+//Link GraphQL Dengan Data Dari DB Postgre 
+app.use('/graphpostgre', graphqlHTTP({
+    schema:schemafilepostgre, //File Schema Yang Dituju
+    graphiql:true, //Untuk mengaktifkan GUI GraphQL pada WEB 
+}));
+//------------------------------
 
-// //Link GraphQL Dengan Data Dari DB MongoDB
-// app.use('/graphmongo', graphqlHTTP({
-//     schema:schemafilemongo, //File Schema Yang Dituju
-//     graphiql:true, //Untuk mengaktifkan GUI GraphQL pada WEB 
-// }));
-// //------------------------------
+//Link GraphQL Dengan Data Dari DB MongoDB
+app.use('/graphmongo', graphqlHTTP({
+    schema:schemafilemongo, //File Schema Yang Dituju
+    graphiql:true, //Untuk mengaktifkan GUI GraphQL pada WEB 
+}));
+//------------------------------
 
 app.listen(3000, () =>{ //Menjalankan Serve Node.JS Pada Port 3000 (localhost:3000)
     return console.log('Request On Port 3000');
 });
 
-// //Link GraphQL Dengan Data Dummy 
-// app.use('/graph', graphqlHTTP({
-//     schema:schemafile, //File Schema Yang Dituju
-//     graphiql:true //Untuk mengaktifkan GUI GraphQL pada WEB 
-// }));
-// //------------------------------
+//Link GraphQL Dengan Data Dummy 
+app.use('/graph', graphqlHTTP({
+    schema:schemafile, //File Schema Yang Dituju
+    graphiql:true //Untuk mengaktifkan GUI GraphQL pada WEB 
+}));
+//------------------------------
